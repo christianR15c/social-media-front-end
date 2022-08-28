fb8d8de191fd957d51310b7bdfab5a18fcab38b5
christian <habinezac15@gmail.com>
2022-08-28 22:23:23 +0200

    ft: sidebar added

---
+++ src/data/dummy.js
@@ -0,0 +1,40 @@
+import { Bookmark, Chat, Event, Group, HelpOutline, PlayCircleFilledOutlined, RssFeed, School, WorkOutline } from "@mui/icons-material";
+
+export const sidebarLinks = [
+    {
+        icon: <RssFeed />,
+        spanText: "Feed"
+    },
+    {
+        icon: <Chat />,
+        spanText: "Feed"
+    },
+    {
+        icon: <PlayCircleFilledOutlined />,
+        spanText: "Videos"
+    },
+    {
+        icon: <Group />,
+        spanText: "Groups"
+    },
+    {
+        icon: <Bookmark />,
+        spanText: "Bookmarks"
+    },
+    {
+        icon: <HelpOutline />,
+        spanText: "Questions"
+    },
+    {
+        icon: <WorkOutline />,
+        spanText: "Jobs"
+    },
+    {
+        icon: <Event />,
+        spanText: "Events"
+    },
+    {
+        icon: <School />,
+        spanText: "Courses"
+    },
+]
